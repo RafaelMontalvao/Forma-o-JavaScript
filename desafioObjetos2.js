56063483feb5500c6c01f7aa9f6601869596a5f1
RafaelMontalvao <rushbig@hotmail.com>
2023-02-17 18:55:18 -0300

    corrigindo desafio2 orientacao a objeto

--- desafioObjetos2.js
+++ desafioObjetos2.js
@@ -19,12 +19,27 @@
 
         calcularImc(){
            return this.peso/(this.altura*this.altura)
+        }
 
-
+        classificarImc(){
+            const imc = this.calcularImc();
+               if(imc < 18.5){
+                    return('!!!! ABAIXO DO PESO !!!!');
+                }
+                else if(imc>=18.5 && imc<25){
+                    return ('Parabéns voce esta com o peso normal.');
+                }else if(imc>=25 && imc<30){
+                    return ('Acima do Peso.');
+                }else if(imc>=30 && imc<40){
+                    return ('!! OBESO !!');
+                }else{
+                    return('!!!!!! OBESIDADE GRAVE !!!!!!');
+                }
+            }
         }
        
-    }
+    
     const jose = new Pessoa("José", 70, 1.77);
-    console.log(jose.calcularImc());
+    console.log(jose.classificarImc());
     const rafael = new Pessoa('Rafael', 77, 1.77)
-    console.log(rafael.calcularImc());
+    console.log(rafael.classificarImc());
