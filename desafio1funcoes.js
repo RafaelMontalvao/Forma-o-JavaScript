53d69434b973edacee3cf4baea6559ff9f9cb183
RafaelMontalvao <rushbig@hotmail.com>
2023-02-17 02:22:25 -0300

    treinando funcoes

---
+++ desafio1funcoes.js
@@ -0,0 +1,18 @@
+function escreverMeuNome(nome){
+    console.log ('Meu nome é ' + nome);
+}
+
+function verificarMaiorIdade(idade){
+    if(idade>=18){
+        console.log('Maior de idade')
+    }else{
+        console.log('Menor de Idade');
+    }
+}
+
+escreverMeuNome('Rafael Brito');
+verificarMaiorIdade(36);
+escreverMeuNome('Marcela Teixeira');
+verificarMaiorIdade(32);
+escreverMeuNome('Fernanda Teixeira');
+verificarMaiorIdade(9);
